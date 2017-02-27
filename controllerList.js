0ec50ec7a6f0ad5925a11f59f7cccb32b220e8ea
RAY FISHER <futbol11usa@gmail.com>
2017-02-27 11:35:28 -0700

    added image

--- controllerList.js
+++ controllerList.js
@@ -2,22 +2,57 @@
  * Created by rayfisher on 2/5/17.
  */
 (function () {
-    angular.module("ToDo", [])
-        .controller("controllerList", function ($scope) {
+    angular.module("ToDo", ['ui.router'])
+        .controller("controllerList", function ($scope,$filter, ToDoService) {
             $scope.todos = [
-                {'title': 'select a stock', }
+                {'title': 'select a stock', done: false}
             ];
+            $scope.lists= ToDoService.lists;
+
+       //     var lists = []
+       //     lists.push({name: userinput.listName, list:[]})
+
+            $scope.addLists= function() {
+                ToDoService.createNewList($scope.newList);
+            }
+         //   console.log(ToDoService.lists);
+
+         //   console.log(ToDoService.lists);
 
             $scope.addTodo = function () {
-                $scope.todos.push({'title': $scope.newToDo});
+                $scope.currentList.todos.push({'title': $scope.newToDo, done: false});
                 $scope.newToDo = ''
             };
 
+            $scope.setCurrentList = function (list) {
+                $scope.currentList = list
+            }
             $scope.clearTasks = function () {
+                var filterArray= $filter('filter')($scope.todos,{done:false});
+                $scope.todos= filterArray
             };
-
             $scope.todos = $scope.todos.filter(function(item) {
                 return !item.done
             });
+        })
+        .service('ToDoService', function(){
+            var lists=[];
+                        var service= {
+                            lists:lists,
+                createNewList: createNewList,
+                addTodo: addTodo
+            }
+            function createNewList(listName) {
+                lists.push({
+                    name: listName,
+                    todos: []
+                })
+            }
+            function addTodo() {
+
+            }
+            return service
         });
 })();
+
+
--- router.js
+++ router.js
@@ -2,22 +2,22 @@
  * Created by rayfisher on 2/7/17.
  */
 
-var routerApp = angular.module("routerApp", ["ui.router"]);
-routerapp.config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/home');
+var ToDo= angular
+    .module("ToDo")
+    .config(function($stateProvider){
 
     $stateProvider
         .state("pro", {
             url: "/pro",
-            templateUrl : "<h1>Pro</h1><p>investing in the future</p>
+            templateUrl : 'pro.html'
         })
         .state("aboutUs", {
             url: "/aboutUs",
-            templateUrl : "<h1>about us</h1><p>this is pro stock options</p>"
+            templateUrl : 'aboutUs.html'
         })
         .state("options", {
             url: "/options",
-            templateUrl : "<h1>options</h1><p>stock option contracts</p>"
+            templateUrl : 'options.html'
         });
 });
 
